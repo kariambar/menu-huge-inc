536b5f6ffadf10ace7aa2470225f00b14bbef38c
kariambar <karina.spalacios@gmail.com>
2017-05-11 12:32:19 -0300

    agregando js y archivo style

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,18 @@
-document.getElementById("toggle").addEventsListener("click", function(event){
-event.preventDefault();
-document.getElementById("nav-header").classList.toggle("open");
-document.getElementById("body").classList.toggle("overflow-hidden");
-
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
+	document.getElementById("nav-header").classList.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");
 });
 
+ var currentScroll = window.pageYOffset || document.body.scrollTop;
+
+  var lastScrollTop = 0;
+  window.addEventListener("scroll", function(){
+  	var currentScroll = window.pageYOffset || document.body.scrollTop;
+  	if(currentScroll > lastScrollTop){
+  		console.log("down");	
+  	} else{
+  		console.log("up");
+  	}
+  	lastScrollTop = currentScroll;
+  });
